fa654af4955e13169801e86cbd0e08703ec742f1
Kody Carling <kodycarling19@gmail.com>
2021-05-21 19:19:48 -0600

    chapter 1 challenge

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    first_name = input("what is you first name? ")
+    last_name = input("what is you last name? ")
+
+    print(f'Your name is {first_name} {last_name}')
+
+
+main()
